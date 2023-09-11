133694bea3ee5569f6f7eee0c0ec88283e4cc39b
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1G.local>
2023-09-10 18:16:10 -0700

    b3: created and used CoroutineRule

--- app/src/test/java/eu/maxkim/boredombuster/activity/ui/newactivity/NewActivityViewModelTest.kt
+++ app/src/test/java/eu/maxkim/boredombuster/activity/ui/newactivity/NewActivityViewModelTest.kt
@@ -4,26 +4,13 @@ import eu.maxkim.boredombuster.activity.fake.usecase.FakeDeleteActivity
 import eu.maxkim.boredombuster.activity.fake.usecase.FakeGetRandomActivity
 import eu.maxkim.boredombuster.activity.fake.usecase.FakeIsActivitySaved
 import eu.maxkim.boredombuster.activity.fake.usecase.FakeSaveActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
-import org.junit.After
-import org.junit.Before
+import eu.maxkim.boredombuster.util.CoroutineRule
+import org.junit.Rule
 import org.junit.Test
 
 internal class NewActivityViewModelTest {
-    private val testDispatcher = StandardTestDispatcher()
-
-    @Before
-    fun setUp() {
-        Dispatchers.setMain(testDispatcher)
-    }
-
-    @After
-    fun tearDown() {
-        Dispatchers.resetMain()
-    }
+    @get:Rule
+    val coroutineRule = CoroutineRule()
 
     @Test
     fun `creating a viewmodel exposes loading ui state`() {
