24e214a7a4eb0747b54c6f7b066373260f180595
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1G.local>
2023-09-13 11:11:41 -0700

    b14: MockWebServer 3 testcases

--- app/src/test/java/eu/maxkim/boredombuster/activity/framework/datasource/ActivityRemoteDataSourceImplTest.kt
+++ app/src/test/java/eu/maxkim/boredombuster/activity/framework/datasource/ActivityRemoteDataSourceImplTest.kt
@@ -1,13 +1,21 @@
 package eu.maxkim.boredombuster.activity.framework.datasource
 
+import com.squareup.moshi.JsonDataException
 import com.squareup.moshi.Moshi
+import eu.maxkim.boredombuster.activity.fake.usecase.responseActivity
 import eu.maxkim.boredombuster.activity.framework.api.ActivityApiClient
 import eu.maxkim.boredombuster.activity.framework.api.ActivityTypeAdapter
+import eu.maxkim.boredombuster.model.Result
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
 import okhttp3.OkHttpClient
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
+import retrofit2.HttpException
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -38,5 +46,65 @@ class ActivityRemoteDataSourceImplTest {
     @After
     fun shutdownServer() {
         mockWebServer.shutdown()
+    }
+
+    // test that the correct JSON is parsed successfully into an Activity
+    @Test
+    fun `correct response is parsed into success result`() = runTest {
+        // Arrange
+        val response = MockResponse()
+            .setBody(successfulResponse)
+            .setResponseCode(200)
+
+        mockWebServer.enqueue(response)
+
+        val activityRemoteDataSource = ActivityRemoteDataSourceImpl(apiClient)
+        val expectedActivity = responseActivity
+
+        // Act
+        val result = activityRemoteDataSource.getActivity()
+
+        // Assert
+        assert(result is Result.Success)
+        assertEquals((result as Result.Success).data, expectedActivity)
+    }
+
+    // simulate a faulty response upon malformed JSON and 400 response code
+    @Test
+    fun `malformed response returns json error result`() = runTest {
+        // Arrange
+        val response = MockResponse()
+            .setBody(errorResponse)
+            .setResponseCode(200)
+
+        mockWebServer.enqueue(response)
+
+        val activityRemoteDataSource = ActivityRemoteDataSourceImpl(apiClient)
+
+        // Act
+        val result = activityRemoteDataSource.getActivity()
+
+        // Assert
+        assert(result is Result.Error)
+        assert((result as Result.Error).error is JsonDataException)
+    }
+
+    @Test
+    fun `error response returns http error result`() = runTest {
+        // Arrange
+        val response = MockResponse()
+            .setBody(errorResponse)
+            .setResponseCode(400)
+
+        mockWebServer.enqueue(response)
+
+        val activityRemoteDataSource = ActivityRemoteDataSourceImpl(apiClient)
+
+        // Act
+        val result = activityRemoteDataSource.getActivity()
+
+        // Assert
+        assert(result is Result.Error)
+        assert((result as Result.Error).error is HttpException)
     }
 }
